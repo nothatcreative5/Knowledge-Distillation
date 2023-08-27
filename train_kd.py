b592bf3939b2fb989833c8a5c4e1b20995a5fcb3
unknown <nothatcreative5@gmail.com>
2023-08-28 00:41:57 +0330

    added parameters

--- train_kd.py
+++ train_kd.py
@@ -163,8 +163,7 @@ class Trainer(object):
         test_loss = 0.0
         for i, sample in enumerate(tbar):
             image, target = sample['image'], sample['label']
-
-            print(image.shape)
+            
             if self.args.cuda:
                 image, target = image.cuda(), target.cuda()
             with torch.no_grad():
