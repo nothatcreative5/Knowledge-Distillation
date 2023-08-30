5999d4ba27d323b6edb648c6464698001882ca6f
unknown <nothatcreative5@gmail.com>
2023-08-30 19:10:57 +0330

    added to pascal

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -7,6 +7,7 @@ def make_data_loader(args, **kwargs):
     if args.dataset == 'pascal':
         train_set = pascal.VOCSegmentation(args, split='train')
         val_set = pascal.VOCSegmentation(args, split='val')
+        test_set = cityscapes.CityscapesSegmentation(args, split='test')
         if args.use_sbd:
             sbd_train = sbd.SBDSegmentation(args, split=['train', 'val'])
             train_set = combine_dbs.CombineDBs([train_set, sbd_train], excluded=[val_set])
@@ -14,7 +15,7 @@ def make_data_loader(args, **kwargs):
         num_class = train_set.NUM_CLASSES
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=1, shuffle=False, **kwargs)
-        test_loader = None
+        test_loader = DataLoader(test_set, batch_size=1, shuffle=False, **kwargs)
 
         return train_loader, val_loader, test_loader, num_class
 
