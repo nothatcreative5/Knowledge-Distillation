8c81aa76399586024b0c1fe81fe03aca6686d0ce
unknown <nothatcreative5@gmail.com>
2023-08-31 23:09:09 +0330

    idk at this point

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -15,20 +15,18 @@ def make_data_loader(args, **kwargs):
         num_class = train_set.NUM_CLASSES
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=1, shuffle=False, **kwargs)
-        test_loader = DataLoader(test_set, batch_size=1, shuffle=False, **kwargs)
 
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, val_loader, None, num_class
 
     elif args.dataset == "cityscapes":
         train_set = cityscapes.CityscapesSegmentation(args, split='train')
         val_set = cityscapes.CityscapesSegmentation(args, split='val')
-        test_set = cityscapes.CityscapesSegmentation(args, split='test')
+
         num_class = train_set.NUM_CLASSES
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=1, shuffle=False, **kwargs)
-        test_loader = DataLoader(test_set, batch_size=1, shuffle=False, **kwargs)
         
-        return train_loader, val_loader, test_loader, num_class
+        return train_loader, val_loader, None, num_class
         
     else:
         raise NotImplementedError
